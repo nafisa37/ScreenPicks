d0a7d2b6b845ae8fbdf36c5d59c08007bf16342d
nafisa37 <na569@njit.edu>
2024-02-07 13:09:30 -0500

    making ucid and date visible for problem3

--- public_html/M2_HW/problem3.php
+++ public_html/M2_HW/problem3.php
@@ -10,6 +10,7 @@ function bePositive($arr) {
     //note: use the $arr variable, don't directly touch $a1-$a4
     //TODO use echo to output all of the values as positive (even if they were originally positive) and maintain the original datatype
     //hint: may want to use var_dump() or similar to show final data types
+    //nA569, 2/7/2024
     foreach ($arr as $x){
         if (is_string($x)) {
             //var_dump($x);
